15bba1af2be08f082cf4020adfec3d572769b7d6
Martin Kobetic <mkobetic@gmail.com>
2013-12-10 12:49:04 -0500

    add table test

--- client_test.go
+++ client_test.go
@@ -31,6 +31,25 @@ func TestGetTables(t *testing.T) {
 
 func TestEventStream(t *testing.T) {
 	run(t, func(client Client, table Table) {
+		err := table.Stream(func(stream *EventStream) {
+			var data map[string]interface{}
+			now := time.Now()
+			for i := 0; i < 10; i++ {
+				stream.AddEvent("xyz", NewEvent(now.Add((time.Duration(i)*time.Hour)), data))
+			}
+		})
+		if err != nil {
+			t.Fatalf("Failed to create event stream: (%v)", err)
+		}
+		events, err := table.GetEvents("xyz")
+		if err != nil || len(events) != 10 {
+			t.Fatalf("Failed to get 10 events back: %d events, (%v)", len(events), err)
+		}
+	})
+}
+
+func TestTableEventStream(t *testing.T) {
+	run(t, func(client Client, table Table) {
 		err := client.Stream(func(stream *EventStream) {
 			var data map[string]interface{}
 			now := time.Now()
